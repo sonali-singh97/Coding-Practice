fe7a832118b22c825c4747c30218b1f2febc8e10
Sonali Singh <sonali122000@gmail.com>
2021-05-03 23:30:25 +0530

    add reverse string problem

---
+++ Strings/ReverseString.cpp
@@ -0,0 +1,18 @@
+ReverseString.cpp
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int len = s.size();
+        int i =0;
+        int j = len-1;
+        
+        while(i < j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
