65ef84f1dd4ec3273b5111c4ac754d323d437cdf
Sonali Singh <sonali122000@gmail.com>
2021-12-11 22:29:32 +0530

    Add backtracking question

---
+++ DYnamic Programming/MinCountPallindromicPartitioning.cpp
@@ -0,0 +1,69 @@
+/****Question: Leetcode hard***
+ Src: https://leetcode.com/problems/palindrome-partitioning-ii/
+ *******/
+
+class Solution {
+public:
+    int minCut(string s) {
+        int len = s.length();
+        vector<vector<bool>> pal(len, vector<bool>(len));
+        vector<int> dp(len);
+        for (int i =0; i < len; i ++)
+            pal[i][i] = true;
+        
+        for (int i =0; i <len-1; i ++){
+            if(s[i] == s[i+1]){
+                pal[i][i+1] = true;
+            }
+            else pal[i][i+1] = false;
+        }
+        
+        int k = 3;
+        while(k <= len){
+            for(int i = 0; i <= len-k; i ++){
+                int j = i+k-1;
+                if(s[i] == s[j] && pal[i+1][j-1]) pal[i][j] = true;
+                else pal[i][j] = false;
+            }
+            k++;
+        }
+        
+        dp[0] = 0;
+        for (int i =1; i < len; i ++){
+            if (pal[0][i]) {
+                dp[i] = 0;
+                continue;
+            }
+            int minm = INT_MAX;
+            for (int j =i; j > 0; j-- ){
+                if (pal[j][i]){
+                    int res = 1+ dp[j-1];
+                    minm = min(minm, res);
+                }
+            }
+            dp[i] = minm;
+            
+        }
+        
+        return dp[len-1];
+    }
+};
+
+/****** APPROACH******
+Time Complexity: O(N*N)
+Src: https://youtu.be/qmTtAbOTqcg
+
+-> Make separate array for pallindrome and count.
+-> For count make a 1d array, at index 'i' store minm count from 0 to i.
+-> For i in count
+      if string is pallindrome then count[i] = 0
+      else
+        check every suffix, if pallindrome
+            then update minm value in variable (1 + count[suffixStart -1])
+         count[i] = minm
+-> Return count[len-1] 
+
+2. Another DP arroach: n^3
+3. Recursion: n! or 2^n
+
+*********************/
---
+++ DYnamic Programming/WeightedJobScheduling.cpp
@@ -0,0 +1,61 @@
+/***** Question: Amazon
+ src: https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+ ****************/
+
+
+class Solution {
+public:
+    
+   static bool comp (vector<int>& a , vector<int>&b){
+        return a[1] < b[1];
+    }
+    
+    int prevInterval(vector<vector<int>> & jobs, int i){
+        int start = 0, end = i -1;
+        while(start <= end){
+            int mid = (start+end)/2;
+            
+            if (jobs[mid][1] <=jobs[i][0] ){
+                if (jobs[mid+1][1] <= jobs[i][0])
+                    start = mid+1;
+                else return mid;
+            }
+            else end = mid-1;
+        }
+        return -1;
+    }
+    
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int n = profit.size();
+        vector<vector<int>> jobs(n, vector<int>(3));
+        
+        for (int i =0; i < n; i ++){
+          jobs[i] = {startTime[i], endTime[i], profit[i]};
+            
+        }
+        
+        sort(jobs.begin(), jobs.end(), comp);
+
+        vector<int> dp(n);
+        dp[0] = jobs[0][2];
+        
+        for (int i =1; i < n; i ++){
+            int incProfit = jobs[i][2];
+            int ind = prevInterval(jobs, i);
+            if (ind != -1){
+                incProfit += dp[ind];
+            }
+            int exProfit = dp[i-1];
+            dp[i] = max(incProfit, exProfit);
+        }
+        
+        return dp[n-1];
+        
+    }
+};
+
+/****** APPROACH: DP with Binary Search
+Time: O(n*n)/// if array is sorted then O(nlogn)
+Src: https://www.geeksforgeeks.org/weighted-job-scheduling-log-n-time/
+
+*************/
